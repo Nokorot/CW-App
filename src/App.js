d8c3761b16cc6ca8bf0e1afca56c878aec635b64
Nokorot <torhaakon2012@hotmail.com>
2025-08-27 19:06:36 +0200

    App icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {useEffect} from 'react';
 import './App.css';
 import {MemoryProvider} from './components/MemoryContext';
 import IndexPage from './components/index_page/IndexPage';
 
 function App() {
+
+  useEffect(() => {
+    const tryLock = () => {
+      const s = window.screen;
+      if (s && s.orientation && s.orientation.lock) {
+        s.orientation.lock("portrait").catch(() => {});
+      }
+      document.removeEventListener("click", tryLock);
+    };
+    // Lock after first user gesture (required by most browsers)
+    document.addEventListener("click", tryLock, { once: true });
+    return () => document.removeEventListener("click", tryLock);
+  }, []);
+
   return (
     <MemoryProvider>
       <IndexPage/>
